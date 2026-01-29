f62c450cd6afb0628b98ada631cb19b644c99977
Henrique Simoes <henrique.simoes@outlook.com>
2026-01-29 10:20:04 -0400

    feat: add protected route middleware

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,10 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import { ClientsList } from "../pages/Clients/ClientsList";
+import { ProtectedRoute } from "./ProtectedRoute";
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<ClientsList />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <ClientsList />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
